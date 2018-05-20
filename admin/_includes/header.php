30e053b17c7a2e7c5c83db535f1e643f42276092
bochiedev <bochiegfx@gmail.com>
2018-05-20 23:20:53 +0300

    admin

---
+++ admin/_includes/header.php
@@ -0,0 +1,398 @@
+<!-- Header -->
+<header class="header">
+
+
+    <!-- Top line -->
+    <div class="top-line">
+
+        <!-- Brand -->
+        <a href="#" class="brand">
+
+            <!-- Big -->
+            <div class="brand-big">
+                <span class="strong">Admin</span>
+            </div>
+            <!-- /Big -->
+
+            <!-- Small -->
+            <div class="brand-small">
+                Admin
+            </div>
+            <!-- /Small -->
+
+        </a>
+        <!-- /Brand -->
+
+        <!-- Menu button -->
+        <div class="menu-button">
+            <a href="#" class="sidebar-toggle menu-toggle open">
+                <div class="menu-icon">
+                    <span></span><span></span><span></span>
+                    <span></span><span></span><span></span>
+                </div>
+            </a>
+        </div>
+        <!-- /Menu button -->
+
+        <!-- Navigation -->
+        <div class="navigation-top">
+
+            <!-- Navigation right -->
+            <ul class="navbar-top navbar-top-right">
+
+                <!-- Notifications -->
+                <li class="dropdown">
+
+                    <!-- Profile avatar -->
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                        <i class="icon icon-left fa fa-bell-o"></i>
+                        <!--<span class="navbar-top-title">Notifications</span>-->
+                        <span class="badge badge-danger">3</span>
+                    </a>
+                    <!-- /Profile avatar -->
+
+                    <!-- Dropdown menu -->
+                    <ul class="dropdown-menu dropdown-menu-custom dropdown-menu-right dropdown-menu-notifications">
+                        <li class="title">Notifications</li>
+                        <li>
+
+                            <!-- Notification item -->
+                            <a href="#" class="notification-item">
+
+                                <!-- Avatar -->
+                                <div class="image avatar">
+                                    <img src="images/avatar-m-01.jpg" alt="">
+                                </div>
+                                <!-- /Avatar -->
+
+                                <!-- Notification body -->
+                                <div class="notification-body">
+                                    <span class="name">Phillip Sandoval</span> liked your post
+                                    <span class="datetime">2 minutes ago</span>
+                                    <span class="state-dot state-dot-online"></span>
+                                </div>
+                                <!-- /Notification body -->
+
+                            </a>
+                            <!-- /Notification item -->
+
+                        </li>
+                        <li>
+
+                            <!-- Notification item -->
+                            <a href="#" class="notification-item">
+
+                                <!-- Avatar -->
+                                <div class="image avatar">
+                                    <img src="images/avatar-f-05.jpg" alt="">
+                                </div>
+                                <!-- /Avatar -->
+
+                                <!-- Notification body -->
+                                <div class="notification-body">
+                                    <span class="name">Mary Riley</span> delegated case
+                                    <span class="datetime">4 hours ago</span>
+                                    <span class="state-dot state-dot-online"></span>
+                                </div>
+                                <!-- /Notification body -->
+
+                            </a>
+                            <!-- /Notification item -->
+
+                        </li>
+                        <li>
+
+                            <!-- Notification item -->
+                            <a href="#" class="notification-item">
+
+                                <!-- Avatar -->
+                                <div class="image avatar">
+                                    <img src="images/avatar-f-01.jpg" alt="">
+                                </div>
+                                <!-- /Avatar -->
+
+                                <!-- Notification body -->
+                                <div class="notification-body">
+                                    <span class="name">Debra Burton</span> commented your post
+                                    <span class="datetime">4 hours ago</span>
+                                    <span class="state-dot state-dot-online"></span>
+                                </div>
+                                <!-- /Notification body -->
+
+                            </a>
+                            <!-- /Notification item -->
+
+                        </li>
+                        <li>
+
+                            <!-- Notification item -->
+                            <a href="#" class="notification-item">
+
+                                <!-- Avatar -->
+                                <div class="image avatar">
+                                    <img src="images/avatar-m-02.jpg" alt="">
+                                </div>
+                                <!-- /Avatar -->
+
+                                <!-- Notification body -->
+                                <div class="notification-body">
+                                    <span class="name">Nathan Nelson</span> followed you
+                                    <span class="datetime">4 hours ago</span>
+                                    <span class="state-dot state-dot-online"></span>
+                                </div>
+                                <!-- /Notification body -->
+
+                            </a>
+                            <!-- /Notification item -->
+
+                        </li>
+                        <li class="footer">
+                            <a href="#">See all notifications</a>
+                        </li>
+                    </ul>
+                    <!-- /Dropdown menu -->
+
+                </li>
+                <!-- /Notifications -->
+
+
+                <!-- Profile -->
+                <li class="dropdown">
+
+                    <!-- Profile avatar -->
+                    <a href="#" class="dropdown-toggle nav-profile" data-toggle="dropdown">
+                        <span class="profile-name">Mary Riley</span>
+                        <span class="caret"></span>
+                        <div class="profile-avatar">
+                            <div class="profile-avatar-image">
+                                <img src="images/avatar-f-05.jpg" alt="">
+                            </div>
+                        </div>
+                    </a>
+                    <!-- /Profile avatar -->
+
+                    <!-- Profile dropdown menu -->
+                    <ul class="dropdown-menu dropdown-menu-right">
+                        <li><a href="#"><i class="icon icon-inline fa fa-address-card-o"></i> Profile</a></li>
+                        <li><a href="#"><i class="icon icon-inline fa fa-tasks"></i> Tasks</a></li>
+                        <li><a href="#"><i class="icon icon-inline fa fa-sign-out"></i> Sign out</a></li>
+                    </ul>
+                    <!-- /Profile dropdown menu -->
+
+                </li>
+                <!-- /Profile -->
+
+            </ul>
+            <!-- /Navigation right -->
+
+        </div>
+        <!-- /Navigation -->
+
+
+    </div>
+    <!-- /Top line -->
+
+
+    <!-- Sidebar -->
+    <div class="sidebar custom-scrollbar">
+        <div class="sidebar-content">
+
+            <!-- Sidebar search -->
+            <div class="sidebar-search">
+                <form method="get" class="sidebar-search-form">
+                    <div class="input-group">
+                        <input type="text" class="form-control" placeholder="Search...">
+                        <div class="input-group-btn">
+                            <button type="button" class="btn btn-default">
+                                <i class="icon fa fa-search"></i>
+                            </button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+            <!-- /Sidebar search -->
+
+            <!-- Navigation -->
+            <ul class="sidebar-navigation sb-nav">
+
+
+                <li class="sb-dropdown">
+                    <a href="#" class="sb-nav-item sb-dropdown-toggle">
+                        <i class="icon fa fa-home"></i>
+                        <span>Dashboard</span>
+                    </a>
+                    <ul class="collapse">
+                        <li>
+                            <a href="index-2.html" class="sb-nav-item">
+                                <i class="icon fa fa-circle-o"></i>
+                                <span>Main layout</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="index-shop.html" class="sb-nav-item">
+                                <i class="icon fa fa-circle-o"></i>
+                                <span>Online shop</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="index-blog.html" class="sb-nav-item">
+                                <i class="icon fa fa-circle-o"></i>
+                                <span>Blog dashboard</span>
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+
+                <li class="sb-dropdown">
+                    <a href="#" class="sb-nav-item sb-dropdown-toggle">
+                        <i class="icon fa fa-leaf"></i>
+                        <span>UI elements</span>
+                    </a>
+                    <ul class="collapse">
+                        <li>
+                            <a href="badges.html" class="sb-nav-item">
+                                <i class="icon fa fa-circle-o"></i> <span>Badges</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="labels.html" class="sb-nav-item">
+                                <i class="icon fa fa-circle-o"></i> <span>Labels</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="list-groups.html" class="sb-nav-item">
+                                <i class="icon fa fa-circle-o"></i> <span>List groups</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="pagination.html" class="sb-nav-item">
+                                <i class="icon fa fa-circle-o"></i> <span>Pagination</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="panels.html" class="sb-nav-item">
+                                <i class="icon fa fa-circle-o"></i> <span>Panels</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="progress-bars.html" class="sb-nav-item">
+                                <i class="icon fa fa-circle-o"></i> <span>Progress bars</span>
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+
+                <li class="sb-dropdown">
+                    <a href="#" class="sb-nav-item sb-dropdown-toggle">
+                        <i class="icon fa fa-code"></i>
+                        <span>Components</span>
+                    </a>
+                    <ul class="collapse">
+                        <li>
+                            <a href="alerts.html" class="sb-nav-item">
+                                <i class="icon fa fa-circle-o"></i> <span>Alerts</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="tooltips.html" class="sb-nav-item">
+                                <i class="icon fa fa-circle-o"></i> <span>Tooltips</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="popovers.html" class="sb-nav-item">
+                                <i class="icon fa fa-circle-o"></i> <span>Popovers</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="modals.html" class="sb-nav-item">
+                                <i class="icon fa fa-circle-o"></i> <span>Modals</span>
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+
+
+                <li>
+                    <a href="buttons.html" class="sb-nav-item">
+                        <i class="icon fa fa-square"></i>
+                        <span>Buttons</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="forms.html" class="sb-nav-item">
+                        <i class="icon fa fa-list-alt"></i>
+                        <span>Forms</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="tables.html" class="sb-nav-item">
+                        <i class="icon fa fa-table"></i>
+                        <span>Tables</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="charts.html" class="sb-nav-item">
+                        <i class="icon fa fa-line-chart"></i>
+                        <span>Charts</span>
+                        <span class="badge badge-danger badge-square">New</span>
+                    </a>
+                </li>
+
+            </ul>
+            <!-- /Navigation -->
+
+            <!-- Sidebar title -->
+            <div class="sidebar-title">Summary information</div>
+            <!-- /Sidebar title -->
+
+            <!-- Summary information -->
+            <div class="sidebar-summary-progress">
+
+                <!-- Progress bar -->
+                <div class="progress-titled mb-0p5">
+                    <div class="progress-title">Pending <span class="value">37%</span></div>
+                    <div class="progress">
+                        <div class="progress-bar progress-bar-primary" role="progressbar"
+                             aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 37%;">
+                        </div>
+                    </div>
+                </div>
+                <!-- /Progress bar -->
+
+                <!-- Progress bar -->
+                <div class="progress-titled mb-0p5">
+                    <div class="progress-title">Approved <span class="value">60%</span></div>
+                    <div class="progress">
+                        <div class="progress-bar progress-bar-success" role="progressbar"
+                             aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 60%;">
+                        </div>
+                    </div>
+                </div>
+                <!-- /Progress bar -->
+
+                <!-- Progress bar -->
+                <div class="progress-titled mb-0p5">
+                    <div class="progress-title">Rejected <span class="value">3%</span></div>
+                    <div class="progress">
+                        <div class="progress-bar progress-bar-danger" role="progressbar"
+                             aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 3%;">
+                        </div>
+                    </div>
+                </div>
+                <!-- /Progress bar -->
+
+            </div>
+            <!-- /Summary information -->
+
+        </div>
+    </div>
+    <!-- /Sidebar -->
+
+
+</header>
+
+
+<!-- /Header -->
